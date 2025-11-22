c5bb9cbfeb301a40647d401445ad3a4e8cf5f08f
Robert Ensign <robert.ensign@lethpolytech.ca>
2025-11-22 11:33:17 -0700

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
       <ul>
         <li>
-          <a href="/">Home</a>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <a href="/popular">Popular</a>
+          <Link to="/popular">Popular</Link>
         </li>
         <li>
-          <a href="/nowplay">Now Playing</a>
+          <Link to="/nowplay">Now Playing</Link>
         </li>
         <li>
-          <a href="/candy">Concession</a>
+          <Link to="/candy">Concession</Link>
         </li>
       </ul>
     </nav>
